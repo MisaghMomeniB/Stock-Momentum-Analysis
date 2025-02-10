bc1d65e70838bdf19d5fa7feae4fc4aace5a8335
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-10 18:22:42 +0330

    Update

--- analysis.py
+++ analysis.py
@@ -6,7 +6,7 @@ from statsmodels.tsa.arima.model import ARIMA
 import plotly.express as px
 
 # Step 1: Load the dataset
-df = pd.read_csv('File Path !')  # Replace with your actual CSV file path
+df = pd.read_csv('Data.csv')  # Replace with your actual CSV file path
 
 # Step 2: Convert 'Date' column to datetime format and handle any errors in conversion
 df['Date'] = pd.to_datetime(df['Date'], errors='coerce')
